d400fe068d105acdcc951d481462227b1520a0f0
aanisha mishra <aanisha.mishra05@gmail.com>
2017-01-21 10:01:43 +0530

    Socket initial implementation

--- app/src/main/java/hackfest/pheonix/haptiq/Activities/AccessCredential.java
+++ app/src/main/java/hackfest/pheonix/haptiq/Activities/AccessCredential.java
@@ -2,14 +2,43 @@ package hackfest.pheonix.haptiq.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 
 import hackfest.pheonix.haptiq.R;
 
 public class AccessCredential extends AppCompatActivity {
 
+    EditText username, password;
+    AutoCompleteTextView url;
+    String[] domain={"Facebook","Gmail","Quora","LinkedIn","GitHub","StackOverFlow","Twitter","Pinterest","Instagram"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_access_credential);
+
+        url = (AutoCompleteTextView) findViewById(R.id.url);
+        username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
+
+        url.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
--- app/src/main/java/hackfest/pheonix/haptiq/Activities/MainActivity.java
+++ app/src/main/java/hackfest/pheonix/haptiq/Activities/MainActivity.java
@@ -1,35 +1,53 @@
 package hackfest.pheonix.haptiq.Activities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.net.URISyntaxException;
 
 import hackfest.pheonix.haptiq.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button scanCode, addCredential;
+    Button scanCode, addCredential, fingerprint;
     IntentIntegrator intentIntegrator;
     TextView qrResult;
+    private Socket mSocket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        try {
+            mSocket = IO.socket("http://52.25.225.108");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        mSocket.connect();
+        mSocket.emit("phone-join");
+        mSocket.on("join-with", new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                Log.e("demo",args[0].toString()+" length "+args[0].toString().length());
+            }
+        });
         scanCode = (Button) findViewById(R.id.scanCode);
-        addCredential= (Button) findViewById(R.id.addCredential);
-        qrResult =(TextView) findViewById(R.id.qrResult);
+        addCredential = (Button) findViewById(R.id.addCredential);
+        fingerprint = (Button) findViewById(R.id.fingerprint);
+        qrResult = (TextView) findViewById(R.id.qrResult);
         intentIntegrator = new IntentIntegrator(this);
         scanCode.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +62,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, AccessCredential.class));
             }
         });
+
+        fingerprint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Fingerprint.class));
+            }
+        });
     }
 
     @Override
@@ -54,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
             if (result.getContents() == null) {
                 Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
             } else {
-                    qrResult.setText(result.getContents());
+                qrResult.setText(result.getContents());
 
             }
         } else {
